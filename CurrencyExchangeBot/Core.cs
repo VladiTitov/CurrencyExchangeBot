5c10e06176389787aff1acbee496b178b4f1f00b
Vladislav Titov <VladiTitov@users.noreply.github.com>
2021-06-10 11:10:14 +0300

    Add project files.

---
+++ CurrencyExchangeBot/Core.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CurrencyExchangeBot
+{
+    class Core
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
